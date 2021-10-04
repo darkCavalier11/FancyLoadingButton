16cdee24e1575fda98d054c149bd5d6e5c34b080
darkCavalier11 <57953014+darkCavalier11@users.noreply.github.com>
2021-10-04 16:04:48 +0530

    added example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,17 +13,52 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   FancyButtonAnimationController _fancyButtonAnimationController =
       FancyButtonAnimationController();
+  String _currentState = 'idle';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
         body: Center(
-          child: FancyLoadingButton(
-            onTap: () {
-              _fancyButtonAnimationController.start();
-            },
-            fancyButtonAnimationController: _fancyButtonAnimationController,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FancyLoadingButton(
+                onTap: () {
+                  _fancyButtonAnimationController.start();
+                },
+                fancyButtonAnimationController: _fancyButtonAnimationController,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              IconButton(
+                onPressed: () {
+                  if (_currentState == 'idle') {
+                    _fancyButtonAnimationController.start();
+                    setState(() {
+                      _currentState = 'play';
+                    });
+                  } else {
+                    _fancyButtonAnimationController.complete();
+                    setState(() {
+                      _currentState = 'idle';
+                    });
+                  }
+                },
+                icon: _currentState == 'idle'
+                    ? Icon(
+                        Icons.play_arrow,
+                        color: Colors.white,
+                        size: 50,
+                      )
+                    : Icon(
+                        Icons.pause_circle,
+                        color: Colors.white,
+                        size: 50,
+                      ),
+              )
+            ],
           ),
         ),
       ),
